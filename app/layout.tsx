c2a4ec478686b86c8fbd2a42ebbc0aeec9ed1400
NullScam <hi@16bitguy.com>
2024-11-03 01:04:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Prabudh's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "Website Developer & Digital Marketing Guy",
 };
 
 export default function RootLayout({
